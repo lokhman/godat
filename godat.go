bb49a5d9fda46d842e0db5637498fda402640914
Alexander Lokhman <alex.lokhman@gmail.com>
2018-03-22 14:56:58 +0000

    Add support for multi-encoding and multi-decoding

--- godat.go
+++ godat.go
@@ -9,65 +9,107 @@ import (
 )
 
 const (
-	tNil = 90 // 'Z'
-
-	tTrue  = 84 // 'T'
-	tFalse = 70 // 'F'
-
-	tInt8  = 73  // 'I'
-	tInt16 = 99  // 'I(+0x1A)'
-	tInt32 = 125 // 'I(+0x34)'
-	tInt64 = 151 // 'I(+0x4E)'
-
-	tUint8  = 85  // 'U'
-	tUint16 = 111 // 'U(+0x1A)'
-	tUint32 = 137 // 'U(+0x34)'
-	tUint64 = 163 // 'U(+0x4E)'
-
-	tFloat32 = 68 // 'D'
-	tFloat64 = 94 // 'D(+0x1A)'
-
-	tString8  = 83  // 'S'
-	tString16 = 109 // 'S(+0x1A)'
-	tString32 = 135 // 'S(+0x34)'
-
-	tArray8  = 65  // 'A'
-	tArray16 = 91  // 'A(+0x1A)'
-	tArray32 = 117 // 'A(+0x34)'
-
-	tObject8  = 79  // 'O'
-	tObject16 = 105 // 'O(+0x1A)'
-	tObject32 = 131 // 'O(+0x34)'
-
-	tBinary8  = 66  // 'B'
-	tBinary16 = 92  // 'B(+0x1A)'
-	tBinary32 = 118 // 'B(+0x34)'
+	t8 byte = 0x1A * iota
+	t16
+	t32
+	t64
 )
 
-func Marshal(v interface{}) ([]byte, error) {
-	buf := new(bytes.Buffer)
-	err := NewEncoder(buf).Encode(v)
-	return buf.Bytes(), err
+const (
+	tNil = 'Z' + t8 // 0x5A
+
+	tTrue  = 'T' + t8 // 0x54
+	tFalse = 'F' + t8 // 0x46
+
+	tInt8  = 'I' + t8  // 0x49
+	tInt16 = 'I' + t16 // 0x63
+	tInt32 = 'I' + t32 // 0x7D
+	tInt64 = 'I' + t64 // 0x97
+
+	tUint8  = 'U' + t8  // 0x55
+	tUint16 = 'U' + t16 // 0x6F
+	tUint32 = 'U' + t32 // 0x89
+	tUint64 = 'U' + t64 // 0xA3
+
+	_        = 'D' + t8  // 0x44
+	_        = 'D' + t16 // 0x5E
+	tFloat32 = 'D' + t32 // 0x78
+	tFloat64 = 'D' + t64 // 0x92
+
+	tString8  = 'S' + t8  // 0x53
+	tString16 = 'S' + t16 // 0x6D
+	tString32 = 'S' + t32 // 0x87
+	_         = 'S' + t64 // 0xA1
+
+	tArray8  = 'A' + t8  // 0x41
+	tArray16 = 'A' + t16 // 0x5B
+	tArray32 = 'A' + t32 // 0x75
+	_        = 'A' + t64 // 0x8F
+
+	tObject8  = 'O' + t8  // 0x4F
+	tObject16 = 'O' + t16 // 0x69
+	tObject32 = 'O' + t32 // 0x83
+	_         = 'O' + t64 // 0x9D
+
+	tBinary8  = 'B' + t8  // 0x42
+	tBinary16 = 'B' + t16 // 0x5C
+	tBinary32 = 'B' + t32 // 0x76
+	_         = 'B' + t64 // 0x90
+)
+
+func encode(enc *Encoder, vv []interface{}) error {
+	for _, v := range vv {
+		if err := enc.Encode(v); err != nil {
+			return err
+		}
+	}
+	return nil
 }
 
-func Dump(filename string, v interface{}) error {
+func Marshal(v interface{}, vv ...interface{}) ([]byte, error) {
+	vv = append([]interface{}{v}, vv...)
+
+	buf := new(bytes.Buffer)
+	if err := encode(NewEncoder(buf), vv); err != nil {
+		return nil, err
+	}
+
+	return buf.Bytes(), nil
+}
+
+func Dump(filename string, v interface{}, vv ...interface{}) error {
+	vv = append([]interface{}{v}, vv...)
+
 	f, err := os.Create(filename)
 	if err != nil {
 		return err
 	}
 	defer f.Close()
-	return NewEncoder(f).Encode(v)
+
+	return encode(NewEncoder(f), vv)
 }
 
-func Unmarshal(data []byte, v interface{}) error {
-	return NewDecoder(bytes.NewReader(data)).Decode(v)
+func decode(dec *Decoder, vv []interface{}) error {
+	for _, v := range vv {
+		if err := dec.Decode(v); err != nil {
+			return err
+		}
+	}
+	return nil
 }
 
-func Load(filename string, v interface{}) error {
+func Unmarshal(data []byte, v interface{}, vv ...interface{}) error {
+	return decode(NewDecoder(bytes.NewReader(data)), append([]interface{}{v}, vv...))
+}
+
+func Load(filename string, v interface{}, vv ...interface{}) error {
+	vv = append([]interface{}{v}, vv...)
+
 	f, err := os.Open(filename)
 	if err != nil {
 		return err
 	}
 	defer f.Close()
-	return NewDecoder(f).Decode(v)
+
+	return decode(NewDecoder(f), vv)
 }
